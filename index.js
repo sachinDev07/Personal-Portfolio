422bee4a216e00a3748510163a2ed591949b62c0
Sachin <sachinkrj077@gmail.com>
2022-09-04 13:10:26 +0500

    slider part complete

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('.slider').slick( {
-        
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass: 'dots'
     });
 });
